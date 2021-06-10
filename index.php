57e1e0ce140ac32f413f16434840aaf78faed3fb
Cecile-29 <cecile.auger.pro@gmail.com>
2021-06-10 10:12:38 +0200

    push exercices PHP partie01

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Projet01 Exercice03</title>
+</head>
+<body>
+<h1>PHP Projet01 Exercice03</h1>
+    <h2>Créez une variable age. Afficher vous êtes majeur ou vous êtes mineur en fonction de l'âge.</h2>
+    <?php
+        $nom = 'lovely';
+        $prenom = 'lili';
+        $age = 55;
+        echo 'Je m\'appelle'. ' '. $prenom. ' '. $nom. ' et j\'ai '. $age. ' '. 'ans.';
+    ?>
+</body>
+</html>
