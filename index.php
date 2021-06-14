4cd588c7950a6cc4f18065ea95251f97646a8bc7
Cecile-29 <cecile.auger.pro@gmail.com>
2021-06-14 16:28:29 +0200

    push exercices PHP

--- index.php
+++ index.php
@@ -5,15 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Projet01 Exercice03</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<h1>PHP Projet01 Exercice03</h1>
-    <h2>Créez une variable age. Afficher vous êtes majeur ou vous êtes mineur en fonction de l'âge.</h2>
-    <?php
-        $nom = 'lovely';
-        $prenom = 'lili';
-        $age = 55;
-        echo 'Je m\'appelle'. ' '. $prenom. ' '. $nom. ' et j\'ai '. $age. ' '. 'ans.';
-    ?>
+    <div class="container-fluid col-7 p-3 m-4 border border-warning rounded justify-content-center">
+        <div class="row ">
+            <h1 class="col-12 text-secondary text-center">PHP Projet01 Exercice03</h1>
+            <h3 class="col-12 text-info text-center">Créez une variable age. Afficher vous êtes majeur ou vous êtes mineur en fonction de l'âge.</h3>
+        </div>
+        <div class="row border border-primary rounded p-3 m-4 text-secondary text-center">
+        <?php
+            $age = 18;
+            if($age >= 18){
+            echo 'Vous êtes majeur !';
+            }else
+             echo 'Vous êtes mineur !';  
+        ?>
+        </div>
+    </div> 
 </body>
 </html>
